425b74a60077f4f5fdbb70e83d411ff3dab4d6fe
Origho-precious <origho9@gmail.com>
2021-03-21 17:10:51 +0100

    feat: adding Board and tile components

---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface IBoardProps {
+	title: string;
+	children: React.ReactNode;
+}
+
+const Board: React.FC<IBoardProps> = ({ children, title }) => {
+	return (
+		<div className="bg-gray-200 w-80 h-full pb-4 rounded mr-6">
+			<h2 className="px-4 py-2 text-center text-black text-lg font-bold">{title}</h2>
+			<article className="mx-2">{children}</article>
+		</div>
+	);
+};
+
+export default Board;
---
+++ src/components/Tile/Tile.tsx
@@ -0,0 +1,10 @@
+
+const Tile = () => {
+	return (
+		<div className='bg-white py-2 px-2 mb-2 transition-shadow shadow-lg rounded'>
+			<p className='text-sm'>Build Trello clone</p>
+		</div>
+	);
+};
+
+export default Tile;
