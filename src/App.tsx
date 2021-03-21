489fda071b11ce7486022fef642923b696a9064d
Origho-precious <origho9@gmail.com>
2021-03-21 17:11:04 +0100

    feat: adding page ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
-import './App.css';
+import Board from "./components/Board/Board";
+import Tile from "./components/Tile/Tile";
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div
+			style={{ background: "#0079bf" }}
+			className="relative pt-4 max-h-screen h-screen"
+		>
+			<div className="w-11/12 mx-auto relative z-50">
+				<h1 className="text-4xl text-blue-300 mb-6 font-bold text-center ">
+					Trello Clone
+				</h1>
+				<div className="flex justify-between ">
+					<Board title="Backlog">
+						<Tile />
+						<Tile />
+						<Tile />
+					</Board>
+					<Board title="TO DOs">
+						<Tile />
+						<Tile />
+						<Tile />
+					</Board>
+					<Board title="DOING">
+						<Tile />
+						<Tile />
+						<Tile />
+					</Board>
+					<Board title="DONE">
+						<Tile />
+						<Tile />
+						<Tile />
+					</Board>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default App;
